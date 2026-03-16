58a9a89dc47316278f9e0ed00d04c1624a4434d9
Syeda Tatheer Fatima <heer15841@gmail.com>
2026-03-16 20:45:48 +0500

    Added some code

---
+++ main.py
@@ -0,0 +1,7 @@
+import json 
+import string
+import random
+from pathlib import Path
+
+class Library:
+    pass
---
+++ web.py
@@ -0,0 +1,4 @@
+import streamlit as st 
+from main import library
+
+st.set_page_config(page_title="🏫Library managment App",Layout="centered")
