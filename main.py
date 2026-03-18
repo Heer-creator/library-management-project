4dffc24ed25922ba5d18afe047375c7015f180aa
Syeda Tatheer Fatima <heer15841@gmail.com>
2026-03-18 12:29:28 +0500

    Made 1st part successfully

--- main.py
+++ main.py
@@ -4,4 +4,56 @@ import random
 from pathlib import Path
 
 class Library:
-    pass
+    database1='books.json'
+    database2='members.json'
+
+    @classmethod
+    def books_data(cls):
+        if Path(cls.database1).exists():
+            with open(cls.database1,'r') as fs:
+                data=json.load(fs)
+                return data
+        return[]
+    
+    @classmethod
+    def members_data(cls):
+        if Path(cls.database2).exists():
+            with open(cls.database2) as fs:
+                data=json.load(fs)
+                return data
+        return []
+    
+    @classmethod
+    def generate_ide(cls):
+        alpha=random.choices(string.ascii_letters,k=3)
+        num=random.choices(string.digits,k=3)
+        ide=alpha+num
+        return "".join(ide)
+    
+    @classmethod
+    def save_member_data(cls,data):
+        with open(cls.database2,'w') as fs:
+            json.dump(data,fs,indent=4)
+
+
+    @classmethod
+    def Add_member(cls,name,email,age,password):
+        data2=cls.members_data()
+        if age<18:
+            return None,"Age must be 18 or above"
+        else:
+            ide=cls.generate_ide()
+            user={
+                "name":name,
+                "email":email,
+                "age":age,
+                "password":password,
+                "ide":ide,
+            }
+        data2.append(user)
+        cls.save_member_data(user)
+        return user,"Account created successfully"
+        
+
+
+
--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-# from main import library
+from main import Library
 from datetime import date
 
 st.set_page_config(page_title="🏫Library management App",layout="centered")
@@ -11,10 +11,21 @@ menu=st.sidebar.selectbox("Choose Action:",["Add member","Delete member","Update
 if menu == "Add member":
     st.subheader("Add a New Member")
     name = st.text_input("Name")
-    email_input = st.text_input("Enter Email")
-    dob=st.date_input("Date of Birth", min_value=date(1900,1,1),max_value=date(2020,1,1))
+    email = st.text_input("Enter Email")
+    age=st.number_input("Enter your age",min_value=1,step=1)
+    password=st.text_input("Create a strong password",type="password")
+
 
     if st.button("Submit"):
-        st.success(f"Member {name} added successfully!")
+        if name and email and age and password:
+            lib=Library()
+            user,msg=lib.Add_member(name,email,int(age),password)
+            st.success(msg)
+            if user:
+                st.write(f"Your member ide is: {user['ide']}")
+        else:
+            st.warning("Fill all fields")
+        
+        
 
 
